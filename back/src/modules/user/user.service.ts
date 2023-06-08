caeb74f1ba88a756bc1577786ea954328a13b423
Jackson93150 <jackjack93150@gmail.com>
2023-06-08 15:52:40 +0200

    feat: sendmail and get allbirthday

--- back/src/modules/user/user.service.ts
+++ back/src/modules/user/user.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/models/user.entity';
 import * as fs from 'fs';
+import { Cron } from '@nestjs/schedule';
+import * as sendgridMail from '@sendgrid/mail';
+import { SUBJECT, BODY } from 'src/constant/sendgrid';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
@@ -44,5 +48,49 @@ export class UserService {
 
   async getUserById(id_birthday: number): Promise<User | undefined> {
     return this.userRepository.findOne({ where: { id_birthday } });
+  }
+
+  async getBirthday(): Promise<User[]> {
+    try {
+      const today = new Date();
+      const month = today.getMonth() + 1;
+      const day = today.getDate();
+
+      const users = await this.userRepository
+        .createQueryBuilder('user')
+        .where(`MONTH(user.birthday) = :month`, { month })
+        .andWhere(`DAY(user.birthday) = :day`, { day })
+        .getMany();
+
+      if (users.length > 0) {
+        return users;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      this.logger.error(
+        `Erreur lors de la récupération des utilisateurs : ${error.message}`,
+      );
+    }
+  }
+
+  @Cron('0 0 * * *')
+  async sendMail(): Promise<void> {
+    try {
+      sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const users = await this.getBirthday();
+      if (users.length > 0) {
+        users.forEach(async (user) => {
+          await sendgridMail.send({
+            from: process.env.SENDGRID_SENDER,
+            to: user.email,
+            subject: SUBJECT,
+            html: BODY(user),
+          });
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
