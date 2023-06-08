65fd7a50e8bc1860fdc2e5014a68866eb9a94637
iharoun <islem.haroun@my-digital-school.org>
2023-06-08 11:22:15 +0200

    added asset folder + component

---
+++ front/src/component/Header.tsx
@@ -0,0 +1,12 @@
+import '../assets/header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+        <p>
+          Edit <code>src/header.tsx</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../component/Header';
 
 function HomePage() {
   return (
     <div className="HomePage">
+        <Header/>
         <p>
           Edit <code>src/HomePage.tsx</code> and save to reload.
         </p>
