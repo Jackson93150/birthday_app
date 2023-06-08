39a654e5a9fdeee5e61aebb452e736c8a5e4755c
iharoun <islem.haroun@my-digital-school.org>
2023-06-08 11:15:05 +0200

    set up homepage

---
+++ front/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+        <p>
+          Edit <code>src/HomePage.tsx</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default HomePage;
