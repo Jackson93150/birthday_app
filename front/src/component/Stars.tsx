185af3526d3424e993a57103d6612b485a17fc38
endricklosange <69051334+endricklosange@users.noreply.github.com>
2023-06-20 11:57:40 +0200

    feat(make circule and stars animation

--- front/src/component/Stars.tsx
+++ front/src/component/Stars.tsx
@@ -1,14 +1,15 @@
 import stars from '../assets/images/Stars.svg';
 
-
 interface Props {
   starsName: string;
+  active: string;
 }
 
-export default function Stars({starsName }: Props) {
+export default function Stars({ starsName,active }: Props) {
   return (
-    <div className={starsName} >
+    <div className={`stars ${starsName} ${active}`}>
       <img src={stars} alt={starsName} />
     </div>
   );
 }
+
--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import Header from '../component/Header';
-import CircleImage from '../component/CircleImage';
-import Stars from '../component/Stars';
-import '../assets/stars.css';
-import '../assets/username.css';
+import React, { useState, useEffect } from "react";
+import CircleImage from "../component/CircleImage";
+import Stars from "../component/Stars";
+import "../assets/stars.css";
+import "../assets/homepage.css";
+
 function HomePage() {
+  const [activeStars1, setActiveStars1] = useState(false);
+  const [activeStars2, setActiveStars2] = useState(false);
+  const [activeStars3, setActiveStars3] = useState(false);
+
+  useEffect(() => {
+    const timeout1 = setTimeout(() => {
+      setActiveStars1(true);
+    }, 100); // Ajouter la classe "active" à Stars2 après 0,5 seconde
+
+    const timeout2 = setTimeout(() => {
+      setActiveStars2(true);
+    }, 500); // Ajouter la classe "active" à Stars2 après 0,5 seconde
+
+    const timeout3 = setTimeout(() => {
+      setActiveStars3(true);
+    }, 1200); // Ajouter la classe "active" à Stars3 après 1 seconde
+
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+    };
+  }, []);
+
   return (
     <div className="HomePage">
-        <Header/>
-        <CircleImage/>
-        <Stars starsName={'stars1'} />
-        <Stars starsName={'stars2'} />
-        <Stars starsName={'stars3'} />
+      {/* <Header /> */}
+      <section className="section--left">
+        <CircleImage />
+        {activeStars1 && (
+          <Stars
+            starsName={activeStars1 && "stars1"}
+            active={activeStars1 && "active"}
+          />
+        )}
+        {activeStars2 && (
+          <Stars
+            starsName={activeStars2 && "stars2"}
+            active={activeStars2 && "active"}
+          />
+        )}
+        {activeStars3 && (
+          <Stars
+            starsName={activeStars3 && "stars3"}
+            active={activeStars3 && "active"}
+          />
+        )}
+        <h1>test</h1>
+      </section>
     </div>
   );
 }
